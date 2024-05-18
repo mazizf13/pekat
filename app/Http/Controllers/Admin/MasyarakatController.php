b78a81577076b4a6a41245c7a4d012de6d2fa1cb
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-18 19:58:41 +0700

    feat: create index and community detail pages

--- app/Http/Controllers/Admin/MasyarakatController.php
+++ app/Http/Controllers/Admin/MasyarakatController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Masyarakat;
 use Illuminate\Http\Request;
 
 class MasyarakatController extends Controller
 {
     public function index() {
-        return view('Admin.Masyarakat.index');
+
+        $masyarakat =Masyarakat::all();
+
+        return view('Admin.Masyarakat.index', ['masyarakat' => $masyarakat]);
     }
 
     public function show($nik) {
-        return view('Admin.Masyarakat.show');
+
+        $masyarakat = Masyarakat::where('nik', $nik)->first();
+
+        return view('Admin.Masyarakat.show', ['masyarakat' => $masyarakat]);
     } 
 }
