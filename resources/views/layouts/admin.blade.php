438eada52614f5b97a779e8af44dbbc25371e2e8
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-18 12:47:08 +0700

    feat: crud officer

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,19 @@
         integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         
     @yield('css')
+    <style>
+        .btn-purple {
+            background: #6a70fc;
+            border: 1px solid #6a70fc;
+            color: #fff;    
+        }
+
+        .btn-purple:hover {
+            background: #6a70fc;
+            border: 1px solid #6a70fc;
+            color: #fff;    
+        }
+    </style>
         
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
     
